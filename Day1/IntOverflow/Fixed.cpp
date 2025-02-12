4f4b80e244a7cc8ea09a11d743a9559d812e9403
Sai Teja <121243450+sai-teja-k@users.noreply.github.com>
2025-02-12 13:38:20 +0530

    Update Fixed.cpp

--- Day1/IntOverflow/Fixed.cpp
+++ Day1/IntOverflow/Fixed.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <limits>
 
+int add(int a, int b) {
+    if((b>0 && a> std::numeric_limits<int>::max()-b) || (b<0 && a<std::numeric_limits<int>::min()-b)) 
+    {
+        return 0; //overflow detected
+    }
+    return a + b;
+}
+
+int main() {
+    int x,y;
+    std::cout << "Enter the first integer (x): ";
+    std::cin >> x;
+    std::cout << "Enter the second integer (y): ";
+    std::cin >> y;
+    int result = add(x, y);  
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
