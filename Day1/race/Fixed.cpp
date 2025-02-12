69c961e2f386c6d162f50d05f668e7295f532210
Sai Teja <121243450+sai-teja-k@users.noreply.github.com>
2025-02-12 14:02:09 +0530

    Update Fixed.cpp

--- Day1/race/Fixed.cpp
+++ Day1/race/Fixed.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
 
+std::mutex counterMutex;
+int counter = 0;
+
+void increment() {
+    for (int i = 0; i < 1000000; ++i) {
+        std::lock_guard<std::mutex> lock(counterMutex);
+        ++counter;
+    }
+}
+
+int main() {
+    std::thread t1(increment);
+    std::thread t2(increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Counter: " << counter << std::endl;
+    return 0;
+}
